92ad37123b0ccae1878ef9bcb2d887a776250643
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-24 23:11:32 -0400

    refactor: added Response back with working json deserializer

--- proxy.py
+++ proxy.py
@@ -1,8 +1,8 @@
+import json
+
 import requests
 
-from typing import Tuple
-
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS, cross_origin
 
 app: Flask = Flask(__name__)
@@ -14,41 +14,50 @@ def home() -> str:
     return "Hello!!"
 
 
-def handle_request(url: str) -> Tuple[object, int]:
-    print('given url:', url)
-    if not url:
-        return {'error': 'url is missing'}, 400
+def handle_response(response: object, status_code: int) -> Response:
+    return Response(response=json.dumps(response), status=status_code, content_type='application/json')
+
+
+def handle_request(url: str) -> Response:
+    if url is None:
+        return handle_response(response={'error': 'url is missing'}, status_code=400)
 
     response = requests.get(url)
 
     response_status_code = response.status_code
 
     if response_status_code != 200:
-        return {
-            'message': f'Expected response of url to be 200. Got {response_status_code}',
-        }, 400
+        return handle_response(
+            response={
+                'message': f'Expected response of url to be 200. Got {response_status_code}'
+            },
+            status_code=400
+        )
 
     try:
-        return response.json(), 200
+        return handle_response(response=response.json(), status_code=200)
     except Exception as error:
-        return {
-            'message': f'could not parse json {response.text}',
-            'error': str(error),
-        }, 400
+        return handle_response(
+            response={
+                'message': f'could not parse json {response.text}',
+                'error': str(error)
+            },
+            status_code=400
+        )
 
 
 @app.route('/proxy', methods=['GET'])
 @cross_origin(supports_credentials=True)
-def get_response() -> Tuple[object, int]:
+def get_response() -> Response:
     url: str = request.args.get('url', type=str, default=None)
-    response: Tuple[object, int] = handle_request(url)
+    response: Response = handle_request(url)
     return response
 
 
 @app.route('/proxy/<string:url>', methods=['GET'])
 @cross_origin(supports_credentials=True)
-def get_response_with_url(url: str) -> Tuple[object, int]:
-    response = handle_request(url)
+def get_response_with_url(url: str) -> Response:
+    response: Response = handle_request(url)
     return response
 
 
