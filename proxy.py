2c41f826593544d2d5b742b985f8171a65d34c82
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-24 23:04:54 -0400

    refactor: migrated away from flask response due to response not jsonifying

--- proxy.py
+++ proxy.py
@@ -1,7 +1,8 @@
 import requests
 
+from typing import Tuple
+
 from flask import Flask, request
-from flask import Response
 from flask_cors import CORS, cross_origin
 
 app: Flask = Flask(__name__)
@@ -13,45 +14,40 @@ def home() -> str:
     return "Hello!!"
 
 
-def handle_request(url: str) -> Response:
-    if url is None:
-        return Response(response={'error': 'url is missing'}, status=400)
+def handle_request(url: str) -> Tuple[object, int]:
+    print('given url:', url)
+    if not url:
+        return {'error': 'url is missing'}, 400
 
     response = requests.get(url)
 
     response_status_code = response.status_code
 
     if response_status_code != 200:
-        return Response(
-            response={
-                'message': f'Expected response of url to be 200. Got {response_status_code}'
-            },
-            status=400
-        )
+        return {
+            'message': f'Expected response of url to be 200. Got {response_status_code}',
+        }, 400
 
     try:
-        return Response(response=response.json(), status=200)
+        return response.json(), 200
     except Exception as error:
-        return Response(
-            response={
-                'message': f'could not parse json {response.text}',
-                'error': str(error)
-            },
-            status=400
-        )
+        return {
+            'message': f'could not parse json {response.text}',
+            'error': str(error),
+        }, 400
 
 
-@app.route('/', methods=['GET'])
+@app.route('/proxy', methods=['GET'])
 @cross_origin(supports_credentials=True)
-def get_response() -> Response:
+def get_response() -> Tuple[object, int]:
     url: str = request.args.get('url', type=str, default=None)
-    response: Response = handle_request(url)
+    response: Tuple[object, int] = handle_request(url)
     return response
 
 
-@app.route('/<string:url>', methods=['GET'])
+@app.route('/proxy/<string:url>', methods=['GET'])
 @cross_origin(supports_credentials=True)
-def get_response_with_url(url: str) -> Response:
+def get_response_with_url(url: str) -> Tuple[object, int]:
     response = handle_request(url)
     return response
 
