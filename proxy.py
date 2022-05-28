bb99311be47f038d1be4afbca77d0e2650e30f32
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-28 10:45:06 -0400

    refactor: using scalable management of endpoints

--- proxy.py
+++ proxy.py
@@ -1,57 +1,16 @@
-import json
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
 
-import requests
-
-from flask import Flask, request, Response
-from flask_cors import CORS, cross_origin
+from proxy_request import ProxyRequest
+from root import Root
 
 app: Flask = Flask(__name__)
-CORS(app, support_credentials=True)
+api: Api = Api(app)
 
+CORS(app, support_credentials=True, resources={r"/*": {"origins": "*"}})
 
-@app.route('/')
-def home() -> str:
-    return "Hello!!"
-
-
-def handle_response(response: object, status_code: int) -> Response:
-    return Response(response=json.dumps(response), status=status_code, content_type='application/json')
-
-
-def handle_request(url: str) -> Response:
-    if url is None:
-        return handle_response(response={'error': 'url is missing'}, status_code=400)
-
-    response = requests.get(url)
-
-    response_status_code = response.status_code
-
-    if response_status_code != 200:
-        return handle_response(
-            response={
-                'message': f'Expected response of url to be 200. Got {response_status_code}'
-            },
-            status_code=400
-        )
-
-    try:
-        return handle_response(response=response.json(), status_code=200)
-    except Exception as error:
-        return handle_response(
-            response={
-                'message': f'could not parse json {response.text}',
-                'error': str(error)
-            },
-            status_code=400
-        )
-
-
-@app.route('/proxy', methods=['GET'])
-@cross_origin(supports_credentials=True)
-def get_response() -> Response:
-    url: str = request.args.get('url', type=str, default=None)
-    response: Response = handle_request(url)
-    return response
-
+api.add_resource(Root, '/')
+api.add_resource(ProxyRequest, '/proxy')
 
 app.run(host='0.0.0.0', port=8000)
